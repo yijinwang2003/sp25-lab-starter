6e267ef861496489501b6ee0d45b875fee7fdd99
Class Account <cs61c-alc@hive2.CS.Berkeley.EDU>
2025-04-29 18:39:12 -0700

    final update

--- lab07/ex2.c
+++ lab07/ex2.c
@@ -12,8 +12,14 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
-    for (int i = 0; i < arr_size; i++)
-        global_sum += x[i] * y[i];
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++) {
+        #pragma omp critical
+        {
+            global_sum += x[i] * y[i];
+        }
+    }
+    
     return global_sum;
 }
 
@@ -22,17 +28,11 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 0; i < arr_size; i++) {
-            double product = x[i] * y[i];
-            #pragma omp critical
-            {
-                global_sum += product;
-            }
-        }
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
     }
+    
     return global_sum;
 }
 
@@ -44,16 +44,17 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     #pragma omp parallel
     {
         double local_sum = 0.0;
-        // TODO: Parallel Section
+        
         #pragma omp for
         for (int i = 0; i < arr_size; i++) {
             local_sum += x[i] * y[i];
         }
-        // TODO: Critical Section
+        
         #pragma omp critical
         {
             global_sum += local_sum;
         }
     }
+    
     return global_sum;
 }
